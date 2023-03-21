4d2694eec145793b95e3878b61c60b4bb4997b8b
wownd8796 <wownd8796@gmail.com>
2023-03-21 16:16:37 +0900

    20230321

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,69 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>hello, {{ title }}</h1>
+    <div v-html="htmlTag"></div>
+    <figure><img :src="imgUrl" /></figure>
+    <div class="menu" :class="{ active: isActive }" @click="toggle">
+      토글메뉴
+    </div>
+    <ul>
+      <li v-for="(v, k) in data" :key="k">
+        <!--name이 리액트만-->
+        <!-- <p v-if="v.name == 'react'">{{ v.name }}</p> -->
+        <!--name이 리액트만-->
+        {{ v.name }}
+      </li>
+    </ul>
+    <div class="ip">
+      <input type="text" v-model="elInput[0]" />
+      <input type="text" v-model="elInput[1]" />
+
+      <button @click="result">확인</button>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      title: "world!",
+      htmlTag: '<p style="color:red"> v-html </p>',
+      imgUrl: require("../assets/img1.jpg"),
+      isActive: true,
+      data: [
+        { id: 11, name: "react" },
+        { id: 22, name: "vue" },
+      ],
+
+      elInput: ["", ""],
+    };
+  },
+  methods: {
+    result() {
+      console.log(this.elInput);
+    },
+    toggle() {
+      console.log(document.querySelector(".menu"));
+      this.isActive = !this.isActive;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+/* #app{
+
+} */
+.menu {
+  font-size: 3rem;
+  &.active {
+    color: red;
   }
 }
-</script>
+img {
+  width: 300px;
+}
+</style>
---
+++ src/views/TestView.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="testview">
+    <h1>{{ title }}</h1>
+    <div v-html="htmlTag"></div>
+    <div class="content">
+      <figure class="imgBox" v-for="(v, k) in data" :key="k">
+        <img :src="v.url" alt="" />
+        <p>{{ v.id }}</p>
+      </figure>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      title: "Photo List",
+      htmlTag: "존나어렵다!!!!!!!",
+      data: [
+        { id: "이미지1", url: "../img1.jpg" },
+        { id: "이미지2", url: "../img2.jpg" },
+        { id: "이미지3", url: "../img3.jpg" },
+        { id: "이미지4", url: "../img4.jpg" },
+        { id: "이미지5", url: "../img5.jpg" },
+        { id: "이미지6", url: "../img6.jpg" },
+      ],
+    };
+  },
+};
+</script>
+<style lang="scss">
+.testview {
+  max-width: 1400px;
+  margin: 0 auto;
+}
+.content {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-content: center;
+  .imgBox {
+    width: 300px;
+    height: 400px;
+    img {
+      width: 100%;
+      height: 280px;
+    }
+  }
+}
+</style>
